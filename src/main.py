8cde4499e840467ad2b59a5fb6973981c4940cd7
joliverstrom-cmd <joliverstrom@gmail.com>
2025-11-03 13:27:38 +0100

    TextNode basics added

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode, TextType
+
+def main():
+	print("Hello world")
+
+	test_node = TextNode("try this", TextType.BOLD, "www.123.abc")
+
+	print(test_node)
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+class TextType(Enum):
+    BOLD = "**Bold text**"
+    ITALIC = "_Italic text_"
+    TEXT = "text (plain)"
+    CODE = "`code`"
+    LINK = "[anchor text](url)"
+    IMAGE = "![alt text](url)"
+
+
+class TextNode():
+    
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = TextType(text_type)
+        self.url = url
+
+    def __eq__(self, other):
+        if (self.text == other.text 
+            and self.text_type == other.text_type 
+            and self.url == other.url):
+            return True
+        return False
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
